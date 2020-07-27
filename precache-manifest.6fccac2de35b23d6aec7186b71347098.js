069fd36187fbff3070c07abb3f49c8e6469f58bc
Nahian Alam <nahian18@outlook.com>
2020-07-27 07:57:29 -0500

    Updates

---
+++ precache-manifest.6fccac2de35b23d6aec7186b71347098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1da8aafca92d1963b4fe7bbf8117075",
+    "url": "/portfolioV2/index.html"
+  },
+  {
+    "revision": "4eae881d06833a40be66",
+    "url": "/portfolioV2/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "5ad7e693d37e23dd2ca5",
+    "url": "/portfolioV2/static/css/main.3d0a5ebd.chunk.css"
+  },
+  {
+    "revision": "4eae881d06833a40be66",
+    "url": "/portfolioV2/static/js/2.a03e8644.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolioV2/static/js/2.a03e8644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad7e693d37e23dd2ca5",
+    "url": "/portfolioV2/static/js/main.a051815c.chunk.js"
+  },
+  {
+    "revision": "8bcc375ad5591c74fa77",
+    "url": "/portfolioV2/static/js/runtime-main.a97dee4d.js"
+  }
+]);
